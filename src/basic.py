01751e2d88f710816f29c485d42459e6c704fccb
haoqizhang <fullyouth@163.com>
2025-02-15 18:41:16 +0800

    数据类型和变量

---
+++ src/basic.py
@@ -0,0 +1,73 @@
+# 数据类型
+# 整数
+print(100)
+
+# 浮点数
+print(1.23)
+
+# 字符串
+print('hello')
+print("hello")
+
+# 布尔值
+print(True)
+print(False)
+
+# 空值
+# 空值是Python里一个特殊的值，用None表示
+print(None)
+
+# 变量
+a = 100
+print(a)
+
+# 常量
+PI = 3.1415926
+print(PI)
+
+# 用r''表示''内部的字符串默认不转义
+print(r'\\\t\\')
+# 用'''...'''的格式表示多行内容
+print('''line1
+line2
+line3''')
+
+# 与运算
+print(True and True)
+
+# 或运算
+print(True or False)
+
+# 非运算
+print(not True)
+
+
+# 除法
+print(10 / 3)
+
+# 地板除 永远是整数
+print(10 // 3)
+
+# 取余
+print(10 % 3)
+
+# 乘方
+print(2 ** 5)
+
+# 练习
+print('========练习======')
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'Hello, \"Bart"'
+s5 = r'''Hello,
+Bob!'''
+print(n)
+print(f)
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+print(s5)
